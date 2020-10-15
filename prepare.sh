0feb5b131c89c706822a2df269e3d3ee19d282b6
Nick Paradis <37662026+para0056@users.noreply.github.com>
2020-10-15 15:38:48 -0400

    Update prepare.sh
    Changed self-signed cert subject

--- prepare.sh
+++ prepare.sh
@@ -7,6 +7,6 @@ chmod -R +x ./init
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > ./init/initdb.sql
 echo "done"
 echo "Creating SSL certificates"
-openssl req -nodes -newkey rsa:2048 -new -x509 -keyout nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=DE/ST=BY/L=Hintertupfing/O=Dorfwirt/OU=Theke/CN=www.createyourown.domain/emailAddress=docker@createyourown.domain'
+openssl req -nodes -newkey rsa:2048 -new -x509 -keyout nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=CA/ST=ON/L=OTTAWA/O=SAMPLEORG/OU=SAMPLEOU/CN=www.sample.domain/emailAddress=sample@example.com'
 echo "You can use your own certificates by placing the private key in nginx/ssl/self-ssl.key and the cert in nginx/ssl/self.cert"
 echo "done"
